957c0be9ac4a94d324e3a1b34ade514c998fb5c0
thanaphon <e21bvz@gmail.com>
2024-06-16 20:42:55 +0700

    update | deploy data

--- index.js
+++ index.js
@@ -129,7 +129,7 @@ app.post("/user/create-user", async function (req, res) {
 app.post("/sendLine", async function (req, res) {
   try {
     const authHeader = req.headers.authorization;
-    const data = JSON.parse(req.body);
+    const data = Jreq.body;
     const url = "https://api.line.me/v2/bot/message/push";
     console.log("data >>>>>>>>>", data);
 
